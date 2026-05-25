aa67111be003de949602c89fc074a1d18d50b3cc
ROMULO BARRETO DA SILVA <romubah@gmail.com>
2026-05-25 20:55:39 -0300

    refactor: ajuste do script das os concluidas

--- src/sql/fisc_ptfis_concluidas.sql
+++ src/sql/fisc_ptfis_concluidas.sql
@@ -1,5 +1,5 @@
-SELECT
-    /* INSTALAÇÃO sem zeros à esquerda, como número */
+SELECT DISTINCT
+    /* instalação sem zeros à esquerda, como número */
     TRY_TO_NUMBER(LTRIM(a.instalacao, '0')) AS instalacao,
 
     c.status_comercial AS status,
@@ -7,13 +7,13 @@ SELECT
     c.move_in AS inicio_contr,
     c.move_out AS fim_contr,
 
-    /* NOTA sem zeros à esquerda, como número */
+    /* nota sem zeros à esquerda, como número */
     TRY_TO_NUMBER(LTRIM(a.notificatn, '0')) AS nota,
 
     a.status_ccs AS status_sap,
 
-    /* Datas sem hora */
-    CAST(a.dta_modifica_ns AS DATE) AS data_conclusao,
+    /* datas sem hora */
+    TO_DATE(TO_VARCHAR(f.data_baixa), 'YYYYMMDD') AS data_baixa,
     CAST(b.data_final_servico AS DATE) AS data_fiscalizacao,
 
     d.toi,
@@ -23,13 +23,26 @@ SELECT
     a.workcenter,
     a.grupo,
 
-    /* Irregularidade como número */
-    TRY_TO_NUMBER(LTRIM(e.irregularidade, '0')) AS irreg,
+    /* irregularidade como número */
+    CASE   
+        WHEN e.irregularidade IS NOT NULL
+        THEN TRY_TO_NUMBER(LTRIM(e.irregularidade, '0'))
+        ELSE 300
+    END AS irreg,
 
-    a.cod_parecer AS parecer_tec,
-    a.med_altera_equip,
+    CASE
+        WHEN a.cod_parecer IS NOT NULL 
+        THEN a.cod_parecer
+        ELSE '0001'
+    END AS parecer_tec,
 
-    /* Medidor sem prefixo */
+    CASE
+        WHEN a.med_altera_equip IS NOT NULL
+        THEN a.med_altera_equip
+        ELSE 'MANTEM'
+    END AS med_altera_equip,
+
+    /* medidor sem prefixo */
     CASE
         WHEN c.medidor IS NOT NULL
              AND LEFT(c.medidor, 2) BETWEEN 'AA' AND 'ZZ'
@@ -39,19 +52,30 @@ SELECT
 
     a.usu_bx_medida_fs AS usuario_baixa,
     a.viatura AS placa
+
 FROM sb_perdas.eqtl_rs.gp_fiscalizacoes a
+
 LEFT JOIN eqtlinfo_prd.eqtl_rs.visitas_notas b
     ON TRY_TO_NUMBER(LTRIM(a.notificatn, '0'))
        = TRY_TO_NUMBER(LTRIM(b.nota, '0'))
+
 LEFT JOIN eqtlinfo_prd.eqtl_rs.tab_cadastro c
     ON TRY_TO_NUMBER(LTRIM(a.instalacao, '0'))
        = TRY_TO_NUMBER(LTRIM(c.instalacao, '0'))
+
 LEFT JOIN eqtlinfo_prd.eqtl_rs.info_gerais_notas d
     ON TRY_TO_NUMBER(LTRIM(a.notificatn, '0'))
-    = TRY_TO_NUMBER(LTRIM(d.nota, '0'))
+       = TRY_TO_NUMBER(LTRIM(d.nota, '0'))
+
 LEFT JOIN eqtlinfo_prd.eqtl_rs.notas_fiscalizacao e
     ON TRY_TO_NUMBER(LTRIM(a.notificatn, '0'))
-    = TRY_TO_NUMBER(LTRIM(e.nota, '0'))
+       = TRY_TO_NUMBER(LTRIM(e.nota, '0'))
+
+INNER JOIN eqtlinfo_prd.eqtl_rs.acoes_perdas f
+    ON TRY_TO_NUMBER(LTRIM(a.notificatn, '0'))
+       = TRY_TO_NUMBER(LTRIM(f.nota, '0'))
 
 WHERE a.status_ccs = 'FINL'
-  AND a.tipo_nota = 'FS';
+  AND a.tipo_nota = 'FS'
+
+ORDER BY data_baixa DESC;
